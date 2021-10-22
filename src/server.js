48b2d8b0d21ca20b99670de53bd72ce142aba64e
nmhung2022 <nmhung2022@gmail.com>
2021-10-22 13:14:40 +0700

    Config babeljs

--- src/server.js
+++ src/server.js
@@ -1,12 +1,14 @@
-import express from "express";
+import express from 'express';
+import { mapOder } from '*/utilities/sorts';
 const app = express();
 
-app.get("/", (req, res) => {
-  res.end("<h1>Hello world</h1>");
+app.get('/', (req, res) => {
+  res.end('<h1>Hello world</h1>');
 });
 
-const hostname = "localhost";
+const hostname = 'localhost';
 const port = 8000;
 app.listen(port, hostname, () => {
-  console.log("Server running on ", `http://${hostname}:${port}`);
+  // eslint-disable-next-line no-console
+  console.log('Server running on ', `http://${hostname}:${port}`);
 });
