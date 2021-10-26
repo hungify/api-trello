fba194b3b7523289d8a4018dfbdc432a60bccd48
nmhung2022 <nmhung2022@gmail.com>
2021-10-26 20:13:35 +0700

    fix issue Cappitalized Card.controller

--- src/routes/v1/card.route.js
+++ src/routes/v1/card.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { CardController } from '~/controllers/Card.controller';
+import { CardController } from '~/controllers/card.controller';
 import { CardValidation } from '~/validations/card.validation';
 
 const router = express.Router();
