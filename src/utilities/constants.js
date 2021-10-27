2c11bfab4658675a965cf06c9bddf1fdf9acf964
nmhung2022 <nmhung2022@gmail.com>
2021-10-27 17:13:46 +0700

    fix cors

--- src/utilities/constants.js
+++ src/utilities/constants.js
@@ -5,7 +5,4 @@ export const HttpStatusCode = {
   NOT_FOUND: 404,
   INTERNAL_SERVER: 500,
 };
-export const WHITE_LIST_DOMAIN = [
-  'http://localhost:3000',
-  'https://trello-clone-web-e3ba1.web.app/',
-];
+export const WHITE_LIST_DOMAIN = ['https://trello-clone-web-e3ba1.web.app/'];
